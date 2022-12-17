a4aa2a045ce9da53b334e0b8fee2f48de371fb3c
JeanPaulCG28 <castrogallardo28jeanpaul@gmail.com>
2022-12-17 09:22:06 -0500

    .

--- front/ec-final/src/principal/ComponentePrincipal.tsx
+++ front/ec-final/src/principal/ComponentePrincipal.tsx
@@ -1,8 +1,10 @@
 export default function ComponentePrincipal() {
     return (
-        <div className="container">
-            <h1>Bienvenidos</h1>
-            <p></p>
-        </div>
+            <div className="jumbotron jumbotron-fluid bg-warning fluid">
+                <div className="container">
+                    <h1>Bienvenidos</h1>
+                </div>
+            </div>
+            
     );
 }
